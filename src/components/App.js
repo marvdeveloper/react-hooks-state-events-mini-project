e67b2cd874732ebdf8d38c0be505655d79c7e673
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-22 10:23:32 +0300

    add task state, category filter logic, and pass props to child components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,39 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [tasks, setTasks] = useState(TASKS);
+
+  function handleCategorySelect(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleTaskFormSubmit(newTask) {
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeleteTask(taskText) {
+    const updatedTasks = tasks.filter((task) => task.text !== taskText);
+    setTasks(updatedTasks);
+  }
+
+  const filteredTasks = tasks.filter((task) => {
+    return selectedCategory === "All" || task.category === selectedCategory;
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategorySelect={handleCategorySelect}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleTaskFormSubmit}
+      />
+      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
