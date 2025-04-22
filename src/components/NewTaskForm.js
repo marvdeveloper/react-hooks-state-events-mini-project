9810930b67c28984a2bd175326f700ad67a627c6
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-22 10:19:24 +0300

    update NewTaskForm to handle controlled inputs, form submission, and task creation

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categories[0]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { text, category };
+    onTaskFormSubmit(newTask);
+    setText(""); // Clear input field
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
+        <select
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories
+            .filter((category) => category !== "All")
+            .map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+        </select>n
       </label>
       <input type="submit" value="Add task" />
     </form>
